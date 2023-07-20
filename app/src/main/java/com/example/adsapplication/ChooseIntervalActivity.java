939f1620e0089cd3445f6c932674b1e5c5bda9ac
qwq-y <2090078073@qq.com>
2023-07-20 16:02:05 +0800

    range seek bar 用了开源项目，成功跑起来了，但还没实现

--- app/src/main/java/com/example/adsapplication/ChooseIntervalActivity.java
+++ app/src/main/java/com/example/adsapplication/ChooseIntervalActivity.java
@@ -12,7 +12,7 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.VideoView;
 
-import com.example.adsapplication.model.RangeSeekBar;
+import it.sephiroth.android.library.rangeseekbar.RangeSeekBar;
 
 public class ChooseIntervalActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -21,7 +21,6 @@ public class ChooseIntervalActivity extends AppCompatActivity implements View.On
     private TextView textView;
     private VideoView videoView;
     private Button okButton;
-    private SeekBar seekBar;
     private RangeSeekBar rangeSeekBar;
 
     private int videoDuration; // 视频总时长
@@ -38,80 +37,22 @@ public class ChooseIntervalActivity extends AppCompatActivity implements View.On
         okButton = findViewById(R.id.okButton);
         okButton.setOnClickListener(this);
 
-        seekBar = findViewById(R.id.seekBar);
-
-        rangeSeekBar = findViewById(R.id.rangeSeekBar);
-
         videoView = findViewById(R.id.videoView);
         String videoUriStr = getIntent().getStringExtra("videoUriStr");
         videoView.setVideoURI(Uri.parse(videoUriStr));
 
-        // 监听视频准备完成事件
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                // 获取视频的总时长
-                videoDuration = videoView.getDuration();
-                // 设置SeekBar的最大值为视频的总时长
-                seekBar.setMax(videoDuration);
-
-                // 启动视频播放
-                videoView.start();
-
-                // 更新SeekBar的进度
-                updateSeekBarProgress();
-
-                // 监听SeekBar的滑动事件
-                seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                    @Override
-                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        // 将视频播放位置设置为SeekBar的进度
-                        if (fromUser) {
-                            videoView.seekTo(progress);
-                        }
-                    }
-
-                    @Override
-                    public void onStartTrackingTouch(SeekBar seekBar) {
-                        // 暂停视频播放
-                        videoView.pause();
-                    }
-
-                    @Override
-                    public void onStopTrackingTouch(SeekBar seekBar) {
-                        // 继续视频播放
-                        videoView.start();
-                    }
-                });
-
-                // 设置RangeSeekBar的最大值为视频的总时长
-                rangeSeekBar.setMax(videoDuration);
-            }
-        });
-
-        // 监听区间选择事件
+        rangeSeekBar = findViewById(R.id.rangeSeekBar);
         rangeSeekBar.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener() {
             @Override
-            public void onRangeSeekBarValuesChanged(RangeSeekBar bar, int minValue, int maxValue) {
-                /// 更新选择的开始点和结束点
-                startValue = minValue;
-                endValue = maxValue;
-            }
-        });
-    }
+            public void onProgressChanged(
+                    final RangeSeekBar seekBar, final int progressStart, final int progressEnd, final boolean fromUser) { }
 
-    // 更新SeekBar的进度
-    private void updateSeekBarProgress() {
-        seekBar.setProgress(videoView.getCurrentPosition());
-        if (videoView.isPlaying()) {
-            Runnable progressUpdate = new Runnable() {
-                @Override
-                public void run() {
-                    updateSeekBarProgress();
-                }
-            };
-            seekBar.postDelayed(progressUpdate, 100);
-        }
+            @Override
+            public void onStartTrackingTouch(final RangeSeekBar seekBar) { }
+
+            @Override
+            public void onStopTrackingTouch(final RangeSeekBar seekBar) { }
+        });
     }
 
     @Override
