5975630abf4cf9a36e299ea29e44715a0031beeb
qwq-y <2090078073@qq.com>
2023-07-24 16:28:53 +0800

    绘制页面完结（不过没检查笔迹数据）

--- app/src/main/java/com/example/adsapplication/BrushingActivity.java
+++ app/src/main/java/com/example/adsapplication/BrushingActivity.java
@@ -1,5 +1,6 @@
 package com.example.adsapplication;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -9,7 +10,6 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -17,20 +17,21 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import org.json.JSONArray;
+
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BrushingActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String TAG = "ww";
 
     private ImageView imageView;
-    private ImageButton drawButton;
+    private ImageButton brushButton;
     private Button okButton;
     private Button retryButton;
     private TextView textView;
@@ -43,6 +44,10 @@ public class BrushingActivity extends AppCompatActivity implements View.OnClickL
     private Canvas canvas;
     private Path path;
     private Paint paint;
+    private boolean isBrushOn = true;
+
+    private List<List<Float>> pathPointsList = new ArrayList<>();
+    private String pathJsonStr;
 
 
     @Override
@@ -73,8 +78,8 @@ public class BrushingActivity extends AppCompatActivity implements View.OnClickL
 
         textView = findViewById(R.id.textView);
 
-        drawButton = findViewById(R.id.drawButton);
-        drawButton.setOnClickListener(this);
+        brushButton = findViewById(R.id.brushButton);
+        brushButton.setOnClickListener(this);
 
         okButton = findViewById(R.id.okButton);
         okButton.setOnClickListener(this);
@@ -95,52 +100,78 @@ public class BrushingActivity extends AppCompatActivity implements View.OnClickL
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.okButton) {
-            String filePath = Environment.getExternalStorageDirectory() + File.separator + "drawing_data.png";
-            try {
-                FileOutputStream fos = new FileOutputStream(filePath);
-                paintedBitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
-                fos.flush();
-                fos.close();
-                Toast.makeText(BrushingActivity.this, "Drawing saved to " + filePath, Toast.LENGTH_SHORT).show();
-            } catch (Exception e) {
-                e.printStackTrace();
-                Toast.makeText(BrushingActivity.this, "Failed to save drawing", Toast.LENGTH_SHORT).show();
+
+            JSONArray jsonArray = new JSONArray();
+            for (List<Float> point : pathPointsList) {
+                JSONArray pointArray = new JSONArray();
+                pointArray.put(point.get(0));
+                pointArray.put(point.get(1));
+                jsonArray.put(pointArray);
             }
+
+            pathJsonStr = jsonArray.toString();
+
+            Intent intent = new Intent(this, AddMaterialActivity.class);
+            intent.putExtra("pathJsonStr", pathJsonStr);
+            intent.putExtra("frameUriStr", frameUriStr);
+            intent.putExtra("croppedVideoUriStr", croppedVideoUriStr);
+            startActivity(intent);
+
         } else if (v.getId() == R.id.retryButton) {
+
             path.reset();
             paintedBitmap = baseBitmap.copy(Bitmap.Config.ARGB_8888, true);
             canvas = new Canvas(paintedBitmap);
             imageView.setImageBitmap(paintedBitmap);
-        } else if (v.getId() == R.id.drawButton) {
-            imageView.setOnTouchListener(new View.OnTouchListener() {
-                private float[] point = new float[2];
 
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    Matrix matrix = new Matrix();
-                    imageView.getImageMatrix().invert(matrix);
-                    point[0] = event.getX();
-                    point[1] = event.getY();
-                    matrix.mapPoints(point);
+        } else if (v.getId() == R.id.brushButton) {
 
-                    float x = point[0];
-                    float y = point[1];
+            isBrushOn = !isBrushOn;
 
-                    switch (event.getAction()) {
-                        case MotionEvent.ACTION_DOWN:
-                            path.moveTo(x, y);
-                            break;
-                        case MotionEvent.ACTION_MOVE:
-                            path.lineTo(x, y);
-                            canvas.drawPath(path, paint);
-                            break;
-                        case MotionEvent.ACTION_UP:
-                            break;
+            if (isBrushOn) {
+                // 开启绘制功能
+                textView.setText("请绘制生成区域（已拿起画笔）");
+                imageView.setOnTouchListener(new View.OnTouchListener() {
+                    private float[] point = new float[2];
+
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        Matrix matrix = new Matrix();
+                        imageView.getImageMatrix().invert(matrix);
+                        point[0] = event.getX();
+                        point[1] = event.getY();
+                        matrix.mapPoints(point);
+
+                        float x = point[0];
+                        float y = point[1];
+
+                        List<Float> point = new ArrayList<>();
+                        point.add(x);
+                        point.add(y);
+                        pathPointsList.add(point);
+
+                        switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN:
+                                path.moveTo(x, y);
+                                break;
+                            case MotionEvent.ACTION_MOVE:
+                                path.lineTo(x, y);
+                                canvas.drawPath(path, paint);
+                                break;
+                            case MotionEvent.ACTION_UP:
+                                break;
+                        }
+
+                        imageView.invalidate();
+                        return true;
                     }
-                    imageView.invalidate();
-                    return true;
-                }
-            });
+
+                });
+            } else {
+                // 禁用绘制功能
+                imageView.setOnTouchListener(null);
+                textView.setText("请绘制生成区域（已放下画笔）");
+            }
         }
     }
 }
