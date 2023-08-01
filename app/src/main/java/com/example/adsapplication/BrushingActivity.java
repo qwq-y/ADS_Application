7ff7a39275a0afa158ef7e099f4b32b8012b30a6
qwq-y <2090078073@qq.com>
2023-08-01 11:19:28 +0800

    笔刷粗细选择实现了，还没记录数据，界面再优化下

--- app/src/main/java/com/example/adsapplication/BrushingActivity.java
+++ app/src/main/java/com/example/adsapplication/BrushingActivity.java
@@ -1,5 +1,6 @@
 package com.example.adsapplication;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -11,6 +12,7 @@ import android.graphics.Path;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -44,11 +46,12 @@ public class BrushingActivity extends AppCompatActivity implements View.OnClickL
     private Canvas canvas;
     private Path path;
     private Paint paint;
-    private boolean isBrushOn = false;
 
-    private List<List<Float>> pathPointsList = new ArrayList<>();
+    private List<List<Float>> pathPointsList;
     private String pathJsonStr;
 
+    private AlertDialog brushAlertDialog;
+    private float brushWidth = 20f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,14 +90,96 @@ public class BrushingActivity extends AppCompatActivity implements View.OnClickL
         retryButton = findViewById(R.id.retryButton);
         retryButton.setOnClickListener(this);
 
-//        drawingView = findViewById(R.id.drawingView);
+//        btnThick = brushSelectDialog.findViewById(R.id.btnThick);
+//        btnMedium = brushSelectDialog.findViewById(R.id.btnMedium);
+//        btnThin = brushSelectDialog.findViewById(R.id.btnThin);
 
         path = new Path();
+
+        pathPointsList = new ArrayList<>();
+        List<Float> breakPoint = new ArrayList<>();
+        breakPoint.add(-1f);
+        breakPoint.add(-1f);
+        pathPointsList.add(breakPoint);
+
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(Color.BLACK);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(20);
+        paint.setStrokeWidth(brushWidth);
 
+    }
+
+    private void openTouchListener() {
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            private float[] point = new float[2];
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Matrix matrix = new Matrix();
+                imageView.getImageMatrix().invert(matrix);
+                point[0] = event.getX();
+                point[1] = event.getY();
+                matrix.mapPoints(point);
+
+                float x = point[0];
+                float y = point[1];
+
+                List<Float> point = new ArrayList<>();
+                point.add(x);
+                point.add(y);
+                pathPointsList.add(point);
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        path.moveTo(x, y);
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        path.lineTo(x, y);
+                        canvas.drawPath(path, paint);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        List<Float> breakPoint = new ArrayList<>();
+                        breakPoint.add(-1f);
+                        breakPoint.add(-1f);
+                        pathPointsList.add(breakPoint);
+                        break;
+                }
+
+                imageView.invalidate();
+                return true;
+            }
+
+        });
+    }
+
+    private void closeTouchListener() {
+        imageView.setOnTouchListener(null);
+    }
+
+    private void onChooseBrush(int size) {
+        // -1 thin, 0 medium, 1 thick
+        switch (size) {
+            case -1:
+                brushWidth = 5f;
+                textView.setText("已拿起小号画笔");
+                break;
+            case 0:
+                brushWidth = 20f;
+                textView.setText("已拿起中号画笔");
+                break;
+            case 1:
+                brushWidth = 60f;
+                textView.setText("已拿起大号画笔");
+                break;
+        }
+
+        paint.setStrokeWidth(brushWidth);
+
+        openTouchListener();
+
+        if (brushAlertDialog != null && brushAlertDialog.isShowing()) {
+            brushAlertDialog.dismiss();
+        }
     }
 
     @Override
@@ -120,58 +205,58 @@ public class BrushingActivity extends AppCompatActivity implements View.OnClickL
         } else if (v.getId() == R.id.retryButton) {
 
             path.reset();
+
+            pathPointsList = new ArrayList<>();
+            List<Float> breakPoint = new ArrayList<>();
+            breakPoint.add(-1f);
+            breakPoint.add(-1f);
+            pathPointsList.add(breakPoint);
+
             paintedBitmap = baseBitmap.copy(Bitmap.Config.ARGB_8888, true);
             canvas = new Canvas(paintedBitmap);
             imageView.setImageBitmap(paintedBitmap);
 
         } else if (v.getId() == R.id.brushButton) {
 
-            isBrushOn = !isBrushOn;
+            closeTouchListener();
 
-            if (isBrushOn) {
-                // 开启绘制功能
-                textView.setText("请绘制生成区域（已拿起画笔）");
-                imageView.setOnTouchListener(new View.OnTouchListener() {
-                    private float[] point = new float[2];
+            AlertDialog.Builder brushDialog =
+                    new AlertDialog.Builder(BrushingActivity.this);
+            final View dialogView = LayoutInflater.from(BrushingActivity.this)
+                    .inflate(R.layout.popup_brush,null);
 
-                    @Override
-                    public boolean onTouch(View v, MotionEvent event) {
-                        Matrix matrix = new Matrix();
-                        imageView.getImageMatrix().invert(matrix);
-                        point[0] = event.getX();
-                        point[1] = event.getY();
-                        matrix.mapPoints(point);
+            ImageButton thickButton = dialogView.findViewById(R.id.btnThick);
+            ImageButton mediumButton = dialogView.findViewById(R.id.btnMedium);
+            ImageButton thinButton = dialogView.findViewById(R.id.btnThin);
 
-                        float x = point[0];
-                        float y = point[1];
+            thickButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onChooseBrush(1);
+                }
+            });
 
-                        List<Float> point = new ArrayList<>();
-                        point.add(x);
-                        point.add(y);
-                        pathPointsList.add(point);
+            mediumButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onChooseBrush(0);
+                }
+            });
 
-                        switch (event.getAction()) {
-                            case MotionEvent.ACTION_DOWN:
-                                path.moveTo(x, y);
-                                break;
-                            case MotionEvent.ACTION_MOVE:
-                                path.lineTo(x, y);
-                                canvas.drawPath(path, paint);
-                                break;
-                            case MotionEvent.ACTION_UP:
-                                break;
-                        }
+            thinButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onChooseBrush(-1);
+                }
+            });
 
-                        imageView.invalidate();
-                        return true;
-                    }
+            brushDialog.setTitle("请选择笔刷大小");
 
-                });
-            } else {
-                // 禁用绘制功能
-                imageView.setOnTouchListener(null);
-                textView.setText("请绘制生成区域（点击画笔按钮）");
-            }
+            brushDialog.setView(dialogView);
+
+            brushAlertDialog = brushDialog.show();
+
+
         }
     }
 }
